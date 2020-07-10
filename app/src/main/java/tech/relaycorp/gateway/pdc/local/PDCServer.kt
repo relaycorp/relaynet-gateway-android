02e12f126582465de18b0c1892f114e2fc380c42
Gus Narea <gnarea@users.noreply.github.com>
2020-07-10 11:23:30 +0100

    refactor

--- app/src/main/java/tech/relaycorp/gateway/pdc/local/PDCServer.kt
+++ app/src/main/java/tech/relaycorp/gateway/pdc/local/PDCServer.kt
@@ -7,6 +7,7 @@ import io.ktor.http.cio.websocket.Frame
 import io.ktor.http.cio.websocket.close
 import io.ktor.http.cio.websocket.readBytes
 import io.ktor.routing.routing
+import io.ktor.websocket.DefaultWebSocketServerSession
 import io.ktor.websocket.WebSockets
 import io.ktor.websocket.webSocket
 import tech.relaycorp.poweb.handshake.Challenge
@@ -18,44 +19,48 @@ fun Application.main() {
 
     routing {
         webSocket("/v1/parcel-collection") {
-            val nonce = Handshake.generateNonce()
-            val challenge = Challenge(nonce)
-            outgoing.send(Frame.Binary(true, challenge.serialize()))
-
-            val responseRaw = incoming.receive()
-            val response = try {
-                Response.deserialize(responseRaw.readBytes())
-            } catch (_: InvalidMessageException) {
-                close(CloseReason(CloseReason.Codes.CANNOT_ACCEPT, "Invalid handshake response"))
-                return@webSocket
-            }
-
-            if (response.nonceSignatures.isEmpty()) {
-                close(
-                    CloseReason(
-                        CloseReason.Codes.CANNOT_ACCEPT,
-                        "Handshake response did not include any nonce signatures"
-                    )
-                )
-                return@webSocket
-            }
-
-            for (nonceSignature in response.nonceSignatures) {
-                try {
-                    Handshake.verifySignature(nonceSignature, nonce)
-                } catch (_: InvalidHandshakeSignatureException) {
-                    close(
-                        CloseReason(
-                            CloseReason.Codes.CANNOT_ACCEPT,
-                            "Handshake response included invalid nonce signatures"
-                        )
-                    )
-                    return@webSocket
-                }
-            }
+            handshake()
 
             // The actual sending of parcels is part of
             // https://github.com/relaycorp/relaynet-gateway-android/issues/16
+        }
+    }
+}
+
+private suspend fun DefaultWebSocketServerSession.handshake() {
+    val nonce = Handshake.generateNonce()
+    val challenge = Challenge(nonce)
+    outgoing.send(Frame.Binary(true, challenge.serialize()))
+
+    val responseRaw = incoming.receive()
+    val response = try {
+        Response.deserialize(responseRaw.readBytes())
+    } catch (_: InvalidMessageException) {
+        close(CloseReason(CloseReason.Codes.CANNOT_ACCEPT, "Invalid handshake response"))
+        return
+    }
+
+    if (response.nonceSignatures.isEmpty()) {
+        close(
+            CloseReason(
+                CloseReason.Codes.CANNOT_ACCEPT,
+                "Handshake response did not include any nonce signatures"
+            )
+        )
+        return
+    }
+
+    for (nonceSignature in response.nonceSignatures) {
+        try {
+            Handshake.verifySignature(nonceSignature, nonce)
+        } catch (_: InvalidHandshakeSignatureException) {
+            close(
+                CloseReason(
+                    CloseReason.Codes.CANNOT_ACCEPT,
+                    "Handshake response included invalid nonce signatures"
+                )
+            )
+            return
         }
     }
 }
