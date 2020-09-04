b87b3d89176a0dc9276047dd0db4ab25f1545e6f
Gus Narea <gnarea@users.noreply.github.com>
2020-09-04 17:10:15 +0100

    Try desugaring in a dependency

--- app/src/androidTest/java/tech/relaycorp/gateway/AppTest.kt
+++ app/src/androidTest/java/tech/relaycorp/gateway/AppTest.kt
@@ -6,7 +6,10 @@ import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import tech.relaycorp.gateway.test.AppTestProvider.app
+import tech.relaycorp.relaynet.issueGatewayCertificate
+import tech.relaycorp.relaynet.wrappers.generateRSAKeyPair
 import java.time.Instant
+import java.time.ZonedDateTime
 
 @RunWith(AndroidJUnit4::class)
 class AppTest {
@@ -18,5 +21,11 @@ class AppTest {
     @Test
     fun instant() {
         assertNotNull(Instant.now())
+    }
+
+    @Test
+    fun desugaringDependency() {
+        val keyPair = generateRSAKeyPair()
+        issueGatewayCertificate(keyPair.public, keyPair.private, ZonedDateTime.now())
     }
 }
