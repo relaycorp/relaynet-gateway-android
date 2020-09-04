031628abd63eeaa8250601d5e8bb3eb9efbba6b0
Gus Narea <gnarea@users.noreply.github.com>
2020-09-04 15:12:56 +0100

    Use okhttp provider

--- app/src/main/java/tech/relaycorp/gateway/domain/courier/CargoCollection.kt
+++ app/src/main/java/tech/relaycorp/gateway/domain/courier/CargoCollection.kt
@@ -1,20 +1,17 @@
 package tech.relaycorp.gateway.domain.courier
 
-import io.grpc.okhttp.OkHttpChannelBuilder
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
+import tech.relaycorp.cogrpc.okhttp.OkHTTPChannelBuilderProvider
 import tech.relaycorp.gateway.background.ConnectionState
 import tech.relaycorp.gateway.background.ConnectionStateObserver
 import tech.relaycorp.gateway.common.Logging.logger
 import tech.relaycorp.gateway.data.disk.CargoStorage
-import tech.relaycorp.relaynet.cogrpc.client.ChannelBuilderProvider
 import tech.relaycorp.relaynet.cogrpc.client.CogRPCClient
-import java.security.SecureRandom
 import java.util.logging.Level
 import javax.inject.Inject
-import javax.net.ssl.SSLContext
 
 class CargoCollection
 @Inject constructor(
@@ -25,23 +22,12 @@ class CargoCollection
     private val processCargo: ProcessCargo,
     private val processParcels: ProcessParcels
 ) {
-    private val grpcProvider : ChannelBuilderProvider<OkHttpChannelBuilder> = { address, trustManager ->
-        OkHttpChannelBuilder.forAddress(address.hostName, address.port)
-            .let {
-                if (trustManager != null) {
-                    val sslContext = SSLContext.getInstance("TLS")
-                    sslContext.init(null, arrayOf(trustManager), SecureRandom())
-                    it.sslSocketFactory(sslContext.socketFactory)
-                } else {
-                    it
-                }
-            }
-    }
 
     @Throws(Disconnected::class)
     suspend fun collect() {
-        val client =
-            getCourierAddress()?.let { clientBuilder.build(it, grpcProvider) } ?: throw Disconnected()
+        val client = getCourierAddress()?.let {
+            clientBuilder.build(it, OkHTTPChannelBuilderProvider.Companion::makeBuilder)
+        } ?: throw Disconnected()
 
         try {
             client
