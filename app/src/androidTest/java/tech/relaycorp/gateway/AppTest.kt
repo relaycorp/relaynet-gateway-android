58455bda47649aa73348ad65f96d5b932a1cf215
Sérgio Santos <me@sergiosantos.info>
2020-09-04 15:38:03 +0100

    chore: java.time.Instant test

--- app/src/androidTest/java/tech/relaycorp/gateway/AppTest.kt
+++ app/src/androidTest/java/tech/relaycorp/gateway/AppTest.kt
@@ -2,14 +2,21 @@ package tech.relaycorp.gateway
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import tech.relaycorp.gateway.test.AppTestProvider.app
+import java.time.Instant
 
 @RunWith(AndroidJUnit4::class)
 class AppTest {
     @Test
     fun mode() {
         assertEquals(App.Mode.Test, app.mode)
+    }
+
+    @Test
+    fun instant() {
+        assertNotNull(Instant.now())
     }
 }
