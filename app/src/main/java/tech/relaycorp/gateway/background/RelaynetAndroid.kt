68e986fd4569942e9c2704b294e493d1fb6dfdcc
Sérgio Santos <me@sergiosantos.info>
2020-09-10 17:06:44 +0100

    Remove unused constant

--- app/src/main/java/tech/relaycorp/gateway/background/RelaynetAndroid.kt
+++ app/src/main/java/tech/relaycorp/gateway/background/RelaynetAndroid.kt
@@ -2,5 +2,4 @@ package tech.relaycorp.gateway.background
 
 object RelaynetAndroid {
     const val ENDPOINT_NOTIFY_ACTION = "tech.relaycorp.endpoint.NOTIFY"
-    const val GATEWAY_SYNC_PERMISSION = "tech.relaycorp.gateway.SYNC"
 }
