7141d508a59ed6b828305854479caf21c23aaf04
Sérgio Santos <me@sergiosantos.info>
2020-09-29 11:10:29 +0100

    chore: Parcel Delivery instrumented test

---
+++ app/src/androidTest/java/tech/relaycorp/gateway/background/endpoint/GatewaySyncServiceParcelDeliveryTest.kt
@@ -0,0 +1,84 @@
+package tech.relaycorp.gateway.background.endpoint
+
+import android.content.Context
+import android.content.Intent
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.rule.ServiceTestRule
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import tech.relaycorp.gateway.data.database.StoredParcelDao
+import tech.relaycorp.gateway.data.disk.SensitiveStore
+import tech.relaycorp.gateway.data.model.MessageAddress
+import tech.relaycorp.gateway.data.model.RecipientLocation
+import tech.relaycorp.gateway.pdc.local.PDCServer
+import tech.relaycorp.gateway.test.AppTestProvider
+import tech.relaycorp.poweb.PoWebClient
+import tech.relaycorp.poweb.RejectedParcelException
+import tech.relaycorp.relaynet.messages.Parcel
+import tech.relaycorp.relaynet.testing.CertificationPath
+import tech.relaycorp.relaynet.testing.KeyPairSet
+import tech.relaycorp.relaynet.wrappers.x509.Certificate
+import javax.inject.Inject
+
+class GatewaySyncServiceParcelDeliveryTest {
+
+    @get:Rule
+    val serviceRule = ServiceTestRule()
+
+    @Inject
+    lateinit var sensitiveStore: SensitiveStore
+
+    @Inject
+    lateinit var storedParcelDao: StoredParcelDao
+
+    @Before
+    fun setUp() {
+        AppTestProvider.component.inject(this)
+        serviceRule.bindService(
+            Intent(
+                getApplicationContext<Context>(),
+                GatewaySyncService::class.java
+            )
+        )
+    }
+
+    @Test
+    fun parcelDelivery_validParcel() = runBlocking {
+        setGatewayCertificate(CertificationPath.PRIVATE_GW)
+        val recipient = "https://example.org"
+
+        val parcel = Parcel(
+            recipient,
+            ByteArray(0),
+            CertificationPath.PRIVATE_ENDPOINT,
+            senderCertificateChain = setOf(CertificationPath.PRIVATE_GW)
+        ).serialize(KeyPairSet.PRIVATE_ENDPOINT.private)
+
+        PoWebClient.initLocal(PDCServer.PORT).deliverParcel(parcel)
+
+        val storedParcels = storedParcelDao.listForRecipients(
+            listOf(MessageAddress.of(recipient)),
+            RecipientLocation.ExternalGateway
+        ).first()
+        assertEquals(1, storedParcels.size)
+    }
+
+    @Test(expected = RejectedParcelException::class)
+    fun parcelDelivery_invalidParcel() = runBlocking {
+        val parcel = Parcel(
+            "https://example.org",
+            ByteArray(0),
+            CertificationPath.PUBLIC_GW // Wrong certificate to make this parcel invalid
+        ).serialize(KeyPairSet.PUBLIC_GW.private)
+
+        PoWebClient.initLocal(PDCServer.PORT).deliverParcel(parcel)
+    }
+
+    private suspend fun setGatewayCertificate(cert: Certificate) {
+        sensitiveStore.store("local_gateway.certificate", cert.serialize())
+    }
+}
--- app/src/androidTest/java/tech/relaycorp/gateway/test/AppTestComponent.kt
+++ app/src/androidTest/java/tech/relaycorp/gateway/test/AppTestComponent.kt
@@ -3,6 +3,7 @@ package tech.relaycorp.gateway.test
 import dagger.Component
 import tech.relaycorp.gateway.AppModule
 import tech.relaycorp.gateway.background.endpoint.EndpointPreRegistrationServiceTest
+import tech.relaycorp.gateway.background.endpoint.GatewaySyncServiceTest
 import tech.relaycorp.gateway.common.di.AppComponent
 import tech.relaycorp.gateway.data.DataModule
 import javax.inject.Singleton
@@ -15,4 +16,5 @@ interface AppTestComponent : AppComponent {
     // Tests
 
     fun inject(test: EndpointPreRegistrationServiceTest)
+    fun inject(test: GatewaySyncServiceTest)
 }
