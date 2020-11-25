07b697040cdf0ab45a647fb770c0603ef89cd111
Sérgio Santos <me@sergiosantos.info>
2020-11-25 11:26:17 +0000

    fix: Public GW certificate storage (#202)

--- app/src/main/java/tech/relaycorp/gateway/domain/publicsync/PublicSync.kt
+++ app/src/main/java/tech/relaycorp/gateway/domain/publicsync/PublicSync.kt
@@ -77,8 +77,10 @@ class PublicSync
     }
 
     private fun stopSync() {
-        logger.info("Stopping public sync")
-        syncJob?.cancel()
+        if (isSyncing) {
+            logger.info("Stopping public sync")
+            syncJob?.cancel()
+        }
     }
 
     private suspend fun isRegistered() =
