62d6d0d7a2aca6918f698eedd2147f38f4183cf6
Sérgio Santos <me@sergiosantos.info>
2020-11-30 18:11:02 +0000

    Fix remote pings leak

--- app/src/main/java/tech/relaycorp/gateway/background/PingRemoteServer.kt
+++ app/src/main/java/tech/relaycorp/gateway/background/PingRemoteServer.kt
@@ -1,55 +1,37 @@
 package tech.relaycorp.gateway.background
 
+import io.ktor.client.HttpClient
+import io.ktor.client.request.head
+import io.ktor.network.selector.ActorSelectorManager
+import io.ktor.network.sockets.aSocket
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import tech.relaycorp.gateway.common.Logging.logger
 import java.io.IOException
-import java.net.ConnectException
-import java.net.HttpURLConnection
-import java.net.InetSocketAddress
-import java.net.Socket
-import java.net.URL
 import java.util.logging.Level
 import javax.inject.Inject
 
 class PingRemoteServer
 @Inject constructor() {
-    suspend fun pingAddress(address: String, port: Int) =
-        withContext(Dispatchers.IO) {
-            val socket = Socket()
-            try {
-                socket.connect(InetSocketAddress(address, port), TIMEOUT)
-                true
-            } catch (ce: ConnectException) {
-                logger.log(Level.INFO, "Could not reach $address:$port")
-                false
-            } catch (ex: Exception) {
-                logger.log(Level.INFO, "Could not reach $address:$port", ex)
-                false
-            } finally {
-                socket.close()
-            }
+
+    private val ktorClient by lazy { HttpClient() }
+
+    suspend fun pingSocket(address: String, port: Int) =
+        try {
+            aSocket(ActorSelectorManager(Dispatchers.IO))
+                .tcp()
+                .connect(address, port)
+                .use { true }
+        } catch (e: IOException) {
+            logger.log(Level.INFO, "Could not ping $address:$port")
+            false
         }
 
     suspend fun pingHostname(hostname: String) =
-        withContext(Dispatchers.IO) {
-            val connection = URL(hostname).openConnection() as HttpURLConnection
-            try {
-                with(connection) {
-                    setRequestProperty("Connection", "close")
-                    connectTimeout = TIMEOUT
-                    connect()
-                }
-                true
-            } catch (_: IOException) {
-                logger.log(Level.INFO, "Could not ping $hostname")
-                false
-            } finally {
-                connection.disconnect()
-            }
+        try {
+            ktorClient.head<Unit>(hostname)
+            true
+        } catch (e: IOException) {
+            logger.log(Level.INFO, "Could not ping $hostname", e)
+            false
         }
-
-    companion object {
-        private const val TIMEOUT = 2000
-    }
 }
