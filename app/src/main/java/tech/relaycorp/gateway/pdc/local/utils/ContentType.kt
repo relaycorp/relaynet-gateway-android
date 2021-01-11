b969d979234b7fceb3bd56aa0ae5d8141ad9c4d7
Gus Narea <gnarea@users.noreply.github.com>
2021-01-11 15:01:17 +0000

    fix

--- app/src/main/java/tech/relaycorp/gateway/pdc/local/utils/ContentType.kt
+++ app/src/main/java/tech/relaycorp/gateway/pdc/local/utils/ContentType.kt
@@ -1,10 +1,10 @@
 package tech.relaycorp.gateway.pdc.local.utils
 
-import tech.relaycorp.poweb.PoWebContentType
+import tech.relaycorp.relaynet.bindings.ContentTypes
 import io.ktor.http.ContentType as KtorContentType
 
 object ContentType {
-    val PARCEL = KtorContentType.parse(PoWebContentType.PARCEL.value)
-    val REGISTRATION_REQUEST = KtorContentType.parse(PoWebContentType.REGISTRATION_REQUEST.value)
-    val REGISTRATION = KtorContentType.parse(PoWebContentType.REGISTRATION.value)
+    val PARCEL = KtorContentType.parse(ContentTypes.PARCEL.value)
+    val REGISTRATION_REQUEST = KtorContentType.parse(ContentTypes.NODE_REGISTRATION_REQUEST.value)
+    val REGISTRATION = KtorContentType.parse(ContentTypes.NODE_REGISTRATION.value)
 }
