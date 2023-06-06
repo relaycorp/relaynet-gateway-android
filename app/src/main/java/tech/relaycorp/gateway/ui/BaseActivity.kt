617e31dc6f74232e1154ac40225bd26ff5e19ae9
LilianaFaustinoBloco <liliana@bloco.io>
2023-06-06 11:52:50 +0100

    fix: Require Android 6 or newer

--- app/src/main/java/tech/relaycorp/gateway/ui/BaseActivity.kt
+++ app/src/main/java/tech/relaycorp/gateway/ui/BaseActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
 import android.widget.TextView
 import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
@@ -44,10 +43,6 @@ abstract class BaseActivity : AppCompatActivity() {
             window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        }
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            @Suppress("DEPRECATION")
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         }
     }
 
